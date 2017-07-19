8165effc918acc03f07e39a2306eed633b7e108b
jonzlin95 <jonzlin95@gmail.com>
2017-07-19 16:05:40 -0700

    Update httposion

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule Readability.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [{:floki, "~> 0.14.0"},
-     {:httpoison, "~> 0.9.0"},
+     {:httpoison, ">= 0.12.0"},
      {:earmark, "~> 0.1", only: :dev},
      {:ex_doc, "~> 0.11", only: :dev},
      {:credo, "~> 0.3", only: [:dev, :test]},
